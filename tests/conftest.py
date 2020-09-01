130b181e2ee100c74df354e6d66c0e28e8aeadcf
Wolfgang Goritschnig <60265462+w0L-g0R@users.noreply.github.com>
2020-09-01 12:44:09 +0200

    added src. to import statements

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,9 @@
 import pytest
 from pathlib import Path
 import os
-from enspect.paths import file_paths
-from enspect.models.dataset import DataSet
-from enspect.models.workbook import Workbook
+from src.enspect.paths import file_paths
+from src.enspect.models.dataset import DataSet
+from src.enspect.models.workbook import Workbook
 from tests.fixtures_data_structures import *
 from tests.fixtures_IO import *
 import time
